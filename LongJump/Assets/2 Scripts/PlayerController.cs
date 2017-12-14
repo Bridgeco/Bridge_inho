ef596682d283daa3184c8430975a4780805ff1c1
bridgeco <bridgeco0116@gmail.com>
2017-12-14 13:58:27 +0900

    1213
    1213

--- LongJump/Assets/2 Scripts/PlayerController.cs
+++ LongJump/Assets/2 Scripts/PlayerController.cs
@@ -34,7 +34,7 @@ public class PlayerController : MonoBehaviour {
 	private bool isJump = false;
 
 	//현재 스코어
-	public float nowScore;
+	public float nowScore = 0;
 	public float topScore;
 	public GameObject scoreTable;
 	public GameObject playTable;
